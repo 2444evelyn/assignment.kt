67d33cf9e97aad5fe7ced2f2e41e7ab50df7177c
@2444evelyn <everlinetsuma@gmail.com>
2022-02-16 17:52:36 +0300

    kotlin assignment

---
+++ src/assignment .kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var name:String="evelyn tsuma"
+    println(name)
+    var age:Int = 20
+        println(age)
+    var phonenumber : String = "0794034019"
+    println(phonenumber)
+    var weightkg:Int = 68
+    println(weightkg)
+    var citizen:String="kenyan"
+    println(citizen)
+}
+
+
+
